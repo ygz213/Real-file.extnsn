4a3763ec25ac5cddb653c41320a9caf3a543836e
ygz213 <yigitgurbuz0630@outlook.com>
2021-07-29 13:22:27 +0300

    Added 'Select file' button and changes on code

--- app/main.py
+++ app/main.py
@@ -1,74 +1,95 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import filedialog as fd
 from filetype import guess
 
-class main_menu_class():
-    def __init__(self):
-        self.main_menu = Tk()
-        self.main_menu.title('Real-file.extnsn   (R-F.E v3.4)')
-        self.main_menu['bg'] = '#DEDEDE'
-        try:
-            self.main_menu.wm_iconbitmap('icons/icon.ico')
-        except:
-            self.main_menu.wm_iconbitmap('@icons/icon.xbm')
-        try:
-            self.main_menu.state('zoomed')
-        except:
-            self.main_menu.attributes('-fullscreen', True)
+class application():
+	def __init__(self):
+		self.main_menu = Tk()
+		self.main_menu.title('Real-file.extnsn   (R-F.E v3.5)')
+		self.main_menu['bg'] = '#DEDEDE'
+		try:
+			self.main_menu.wm_iconbitmap('icons/icon.ico')
+		except:
+			self.main_menu.wm_iconbitmap('@icons/icon.xbm')
+		try:
+			self.main_menu.state('zoomed')
+		except:
+			self.main_menu.attributes('-fullscreen', True)
+		self.extension_information = StringVar()
+		self.extension_information.set("Select the file or paste the file's path.")
+		self.file_path_frame = Frame()
+
+		self.main_widgets()
 
 
-    def main_widgets(self):
-        extension_information = StringVar()
-        extension_information.set("Paste the file's path.")
-        Label(textvariable = extension_information,   # Changeable string
-              bg = '#DEDEDE',
-              height = 3).pack()
+	def main_widgets(self):
+		Label(textvariable = self.extension_information,   # Changeable string
+			  bg = '#DEDEDE',
+			  height = 3).pack()
 
-        file_path = Entry(justify = 'center',   # Input box for the file's path
-                          width = 50,
-                          bd = 3,
-                          bg = '#D4C4B7',
-                          highlightthickness = 3,
-                          highlightbackground = '#DEDEDE',
-                          highlightcolor = 'black',
-                          selectforeground = 'black',
-                          relief = 'flat')
-        file_path.pack(pady = 13)
+		self.file_path_frame.pack(pady = 13)
 
-        ####### (Scanner function)
-        def scanner():
-            try:
-                scanned_file = guess('{}'.format(file_path.get()))
-                extension_information.set('This file is a {}.'.format(scanned_file.extension.upper()))
-            except FileNotFoundError:
-                messagebox.showerror('ERROR', 'File not found.')
-            except PermissionError:
-                messagebox.showerror('ERROR', 'Permission denied to access file.')
-            except OSError:
-                messagebox.showerror('ERROR', 'Invalid file path.')
-            except AttributeError:
-                messagebox.showerror('ERROR', "Could not find this file's extension. It may be a text file, you can look at the file after renamed as <filename>.txt")
-        #######
+		file_path = Entry(self.file_path_frame,
+						  justify = 'center',   # Input box for the file's path
+						  width = 60,
+						  bd = 4,
+						  bg = '#D4C4B7',
+						  highlightthickness = 2,
+						  highlightbackground = '#DEDEDE',
+						  highlightcolor = '#DEDEDE',
+						  selectforeground = 'black',
+						  relief = 'flat')
+		file_path.pack(side = 'left')
 
-        Button(text = 'SCAN',   # Scanning button
-               font = 18,
-               height = 2,
-               width = 10,
-               bg = '#758E87',
-               activebackground = '#576863',
-               relief = 'flat',
-               command = lambda: scanner()).pack()
+		####### (Select the file and paste to the entry)
+		def get_path():
+			file = fd.askopenfilename()
+			file_path.delete(0, 'end')
+			file_path.insert(0, file)
+		#######
 
-        ####### (License informations)
-        Label(text = '     • Real-file.extnsn is a free software under the MIT License and uses filetype module under the ', bg = '#DEDEDE').pack(side = 'left')
-        MIT_link = Label(text = 'MIT License', bg = '#DEDEDE', fg = 'blue', cursor = 'hand2')
-        MIT_link.bind("<Button-1>", lambda e: ont('https://github.com/h2non/filetype.py/blob/master/LICENSE'))
-        MIT_link.pack(side = 'left')
-        Label(text = '.', bg = '#DEDEDE').pack(side = 'left')
-        #######
-        self.main_menu.mainloop()
+		select_file = Button(self.file_path_frame,
+							 text = 'Select file',
+							 bg = '#758E87',
+							 activebackground = '#576863',
+							 relief = 'flat',
+							 command = lambda: get_path()).pack(side = 'right')
+
+		####### (Scanner function)
+		def scanner():
+			try:
+				scanned_file = guess('{}'.format(file_path.get()))
+				self.extension_information.set('This file is a {}.'.format(scanned_file.extension.upper()))
+			except FileNotFoundError:
+				messagebox.showerror('ERROR', 'File not found.')
+			except PermissionError:
+				messagebox.showerror('ERROR', 'Permission denied to access file.')
+			except OSError:
+				messagebox.showerror('ERROR', 'Invalid file path.')
+			except AttributeError:
+				messagebox.showerror('ERROR', "Could not find this file's extension. It may be a text file, you can look at the file after renamed as <filename>.txt")
+		#######
+
+		Button(text = 'SCAN',   # Scanning button
+			   font = 18,
+			   height = 2,
+			   width = 10,
+			   bg = '#758E87',
+			   activebackground = '#576863',
+			   relief = 'flat',
+			   command = lambda: scanner()).pack()
+
+		####### (License informations)
+		Label(text = '     • Real-file.extnsn is a free software under the MIT License and uses filetype module under the ', bg = '#DEDEDE').pack(side = 'left')
+		MIT_link = Label(text = 'MIT License', bg = '#DEDEDE', fg = 'blue', cursor = 'hand2')
+		MIT_link.bind("<Button-1>", lambda e: ont('https://github.com/h2non/filetype.py/blob/master/LICENSE'))
+		MIT_link.pack(side = 'left')
+		Label(text = '.', bg = '#DEDEDE').pack(side = 'left')
+		#######
+		self.main_menu.mainloop()
+
 
 
 if __name__ == '__main__':
-    user = main_menu_class()
-    user.main_widgets()
+	user = application()
--- app/main_turkish.py
+++ app/main_turkish.py
@@ -1,75 +1,95 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import filedialog as fd
 from filetype import guess
 
-class ana_menü_sınıfı():
-    def __init__(self):
-        self.ana_menü = Tk()
-        self.ana_menü.title('Real-file.extnsn   (R-F.E v3.4)')
-        self.ana_menü['bg'] = '#DEDEDE'
-        try:
-            self.ana_menü.wm_iconbitmap('icons/icon.ico')
-        except:
-            self.ana_menü.wm_iconbitmap('@icons/icon.xbm')
-        try:
-            self.ana_menü.state('zoomed')
-        except:
-            self.ana_menü.attributes('-fullscreen', True)
+class uygulama():
+	def __init__(self):
+		self.ana_menü = Tk()
+		self.ana_menü.title('Real-file.extnsn   (R-F.E v3.5)')
+		self.ana_menü['bg'] = '#DEDEDE'
+		try:
+			self.ana_menü.wm_iconbitmap('icons/icon.ico')
+		except:
+			self.ana_menü.wm_iconbitmap('@icons/icon.xbm')
+		try:
+			self.ana_menü.state('zoomed')
+		except:
+			self.ana_menü.attributes('-fullscreen', True)
+		self.uzantı_bilgilendirmesi = StringVar()
+		self.uzantı_bilgilendirmesi.set('Taranacak dosyanın yolunu yapıştırın.')
+		self.dosya_yolu_kabı = Frame()
+
+		self.ana_widgetlar()
 
 
-    def ana_widgetlar(self):
-        uzantı_bilgilendirmesi = StringVar()
-        uzantı_bilgilendirmesi.set('Taranacak dosyanın yolunu yapıştırın.')
-        Label(textvariable = uzantı_bilgilendirmesi,   # Değişken yazı dizisi
-              bg = '#DEDEDE',
-              height = 3).pack()
+	def ana_widgetlar(self):
+		Label(textvariable = self.uzantı_bilgilendirmesi,   # Değişken yazı dizisi
+			  bg = '#DEDEDE',
+			  height = 3).pack()
 
-        dosya_yolu = Entry(justify = 'center',   # Dosya yolu için giri kutusu
-                           width = 50,
-                           bd = 3,
-                           bg = '#D4C4B7',
-                           highlightthickness = 3,
-                           highlightbackground = '#DEDEDE',
-                           highlightcolor = 'black',
-                           selectforeground = 'black',
-                           relief = 'flat')
-        dosya_yolu.pack(pady = 13)
+		self.dosya_yolu_kabı.pack(pady = 13)
 
-        ####### (Tarayıcı fonksiyonu)
-        def tarayıcı():
-            try:
-                taranan_dosya = guess('{}'.format(dosya_yolu.get()))
-                uzantı_bilgilendirmesi.set('Bu dosya bir {}.'.format(taranan_dosya.extension.upper()))
-            except FileNotFoundError:
-                messagebox.showerror('HATA', 'Dosya bulunamadı.')
-            except OSError:
-                messagebox.showerror('HATA', 'Geçersiz dosya yolu.')
-            except PermissionError:
-                messagebox.showerror('HATA', 'Dosyaya erişim reddedildi.')
-            except AttributeError:
-                messagebox.showerror('HATA', 'Dosyanın uzantısı bulunamadı. Muhtemelen düz bir metin dosyası (Real-file.extnsn düz metin dosyalarının uzantılarını bulamaz), uzantısını bir .TXT dosyasına çevirerek inceleyebilirsiniz.')
-        #######
+		dosya_yolu = Entry(self.dosya_yolu_kabı,
+						   justify = 'center',   # Dosya yolu için giri kutusu
+						   width = 60,
+						   bd = 4,
+						   bg = '#D4C4B7',
+						   highlightthickness = 2,
+						   highlightbackground = '#DEDEDE',
+						   highlightcolor = '#DEDEDE',
+						   selectforeground = 'black',
+						   relief = 'flat')
+		dosya_yolu.pack(side = 'left')
 
-        Button(text = 'TARA',   # Tarama düğmesi
-               font = 18,
-               height = 2,
-               width = 10,
-               bg = '#758E87',
-               activebackground = '#576863',
-               relief = 'flat',
-               command = lambda: tarayıcı()).pack()
+		####### (Dosyayı seç ve giriye yapıştır)
+		def yol_al():
+			dosya = fd.askopenfilename()
+			dosya_yolu.delete(0, 'end')
+			dosya_yolu.insert(0, dosya)
+		#######
 
-        ####### (Lisans bilgilendirmeleri)
-        Label(text = '    • Real-file.extnsn, MIT Lisansı altında özgür bir yazılımdır ve yine', bg = '#DEDEDE').pack(side = 'left')
-        MIT_linki = Label(text = 'MIT Lisansı', bg = '#DEDEDE', fg = 'blue', cursor = 'hand2')
-        MIT_linki.bind("<Button-1>", lambda e: ont('https://github.com/h2non/filetype.py/blob/master/LICENSE'))
-        MIT_linki.pack(side = 'left')
-        Label(text = 'altında olan filetype modülünü kullanır.', bg = '#DEDEDE').pack(side = 'left')
-        #######
-        self.ana_menü.mainloop()
+		dosyayı_seç = Button(self.dosya_yolu_kabı,
+							 text = 'Dosya seç',
+							 bg = '#758E87',
+							 activebackground = '#576863',
+							 relief = 'flat',
+							 command = lambda: yol_al()).pack(side = 'right')
+
+		####### (Tarayıcı fonksiyonu)
+		def tarayıcı():
+			try:
+				taranan_dosya = guess('{}'.format(dosya_yolu.get()))
+				self.uzantı_bilgilendirmesi.set('Bu dosya bir {}.'.format(taranan_dosya.extension.upper()))
+			except FileNotFoundError:
+				messagebox.showerror('HATA', 'Dosya bulunamadı.')
+			except OSError:
+				messagebox.showerror('HATA', 'Geçersiz dosya yolu.')
+			except PermissionError:
+				messagebox.showerror('HATA', 'Dosyaya erişim reddedildi.')
+			except AttributeError:
+				messagebox.showerror('HATA', 'Dosyanın uzantısı bulunamadı. Muhtemelen düz bir metin dosyası (Real-file.extnsn düz metin dosyalarının uzantılarını bulamaz), uzantısını bir .TXT dosyasına çevirerek inceleyebilirsiniz.')
+		#######
+
+		Button(text = 'TARA',   # Tarama düğmesi
+			   font = 18,
+			   height = 2,
+			   width = 10,
+			   bg = '#758E87',
+			   activebackground = '#576863',
+			   relief = 'flat',
+			   command = lambda: tarayıcı()).pack()
+
+		####### (Lisans bilgilendirmeleri)
+		Label(text = '    • Real-file.extnsn, MIT Lisansı altında özgür bir yazılımdır ve yine', bg = '#DEDEDE').pack(side = 'left')
+		MIT_linki = Label(text = 'MIT Lisansı', bg = '#DEDEDE', fg = 'blue', cursor = 'hand2')
+		MIT_linki.bind("<Button-1>", lambda e: ont('https://github.com/h2non/filetype.py/blob/master/LICENSE'))
+		MIT_linki.pack(side = 'left')
+		Label(text = 'altında olan filetype modülünü kullanır.', bg = '#DEDEDE').pack(side = 'left')
+		#######
+		self.ana_menü.mainloop()
 
 
 
 if __name__ == '__main__':
-    kullanıcı = ana_menü_sınıfı()
-    kullanıcı.ana_widgetlar()
+	kullanıcı = uygulama()
